213d52987d66de3aa6002be6f53a820bd5347b25
LeeJay0126 <topaz9889@gmail.com>
2021-10-18 16:10:07 -0700

    .

--- PitaPitScheduleHandler/schedule.py
+++ PitaPitScheduleHandler/schedule.py
@@ -2,9 +2,11 @@
 import sys
 import subprocess
 
+#setting a header for csv file
 print("Enter your Header (Name of your store name or branch name)")
 header = input()
 
+#taking all the employee's name
 def employeeNames():
     print("Enter employees name one by one and enter 'done' when you are finished")
     names = "pete"
@@ -12,33 +14,32 @@ def employeeNames():
     while names != "done":
         print("Enter your employee's name")
         names = input()
-        employeeArray.append(names)
+        if names != "done":
+            employeeArray.append(names)
+        else:
+            names = "done"
     return employeeArray
-    
+
+#combining shifts with employee
 def employeeShift(employeeArray):
     print("Enter employee's shift and date with a comma inbetween (ex. 14:5,Friday)")
     i = 0
     resultArray = []
-    validator = 0
     while i < len(employeeArray):
-        k = 0
         print("Employee Name: " + employeeArray[i])
         shift = ""
         while shift != "done":
-            print("Enter date for the employee in all lower case letter ex. monday")
+            print("Enter date for the employee in all lower case letter and print 'done' when finished ex. monday")
             shift = input()
-            
-            
-            
-
+            if shift != "done":
+                resultArray.append(shift)
+        i += 1
     return resultArray
     
 employeeArray = employeeNames()
 result = employeeShift(employeeArray)
 
-i = 0
-while i < len(result):
-    print(result[i])
+
 
         
         
