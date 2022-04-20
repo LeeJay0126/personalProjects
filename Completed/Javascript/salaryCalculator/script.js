9d1fded65c3d85e574d5be08ab6af7193aa5d5ef
LeeJay0126 <topaz9889@gmail.com>
2022-04-20 14:09:59 -0700

    Salary Calculator Prototype
    Refactored salary calculator project in to a web project. Used HTML, css, and js.
    
    ** Only province of Alberta is implemented

--- Completed/Javascript/salaryCalculator/script.js
+++ Completed/Javascript/salaryCalculator/script.js
@@ -1,3 +1,5 @@
+const provinces = ["Alberta","BC", "Saskatchewan","Manitoba", "Ontario"];
+
 const federal = [
     [50197, 0.15],
     [50195, 0.205],
@@ -8,7 +10,7 @@ const federal = [
 
 const federalMax = 0.33;
 
-const alberta = [
+const Alberta = [
     [131220, 0.1],
     [26344, 0.12],
     [52488, 0.13],
@@ -16,6 +18,8 @@ const alberta = [
 ]
 
 const albertaMax = 0.15;
+
+
 
 function taxCalculator(income, province, maxProvince){
 
@@ -42,5 +46,99 @@ function taxCalculator(income, province, maxProvince){
     }
 
     return owedTax;
+
+}
+
+function provinceSelection(){
+
+    const select = document.getElementById("provinceDropDown").value;
+    // console.log(select);
+
+    // for(i = 0; i < select.options.length; i++){
+    //     if(select.options[i].value == provinces[i]){
+    //         selectedProvince = provinces[i];
+    //     }
+    // }
+
+    let provinceBracket = "";
+
+    switch(select){
+
+        case "Alberta":
+            provinceBracket = Alberta;
+            break;
+        case "BC":
+            provinceBracket = BC;
+            break;
+        case "Saskatchewan":
+            provinceBracket = Saskatchewan;
+            break;
+        case "Manitoba":
+            provinceBracket = Manitoba;
+            break;
+        case "Ontario":
+            provinceBracket = Ontario;
+            break;
+        default:
+            provinceBracket = Alberta;
+
+    }
+
+    return provinceBracket;
+
+}
+
+function maxProvinceSelection(selectedProvince){
+
+    let maxProvince = "";
+
+    switch(selectedProvince){
+
+        case "Alberta":
+            maxProvince = albertaMax;
+            break;
+        case "BC":
+            maxProvince = BC;
+            break;
+        case "Saskatchewan":
+            maxProvince = Saskatchewan;
+            break;
+        case "Manitoba":
+            maxProvince = Manitoba;
+            break;
+        case "Ontario":
+            maxProvince = Ontario;
+            break;
+        default:
+            maxProvince = albertaMax;
+
+    }
+
+    // console.log(maxProvince);
+    return maxProvince;
+
+}
+
+
+function displayResult(){
+
+    let income = document.getElementById("incomeField").value;
+    let selectedProvince = provinceSelection();
+    let maxProvince = maxProvinceSelection(selectedProvince);
+    let federalTax = taxCalculator(income, federal, federalMax);
+    let provincialTax = taxCalculator(income, selectedProvince,maxProvince);
+
+    provincialTax = Number(provincialTax.toFixed(2));
+    federalTax = Number(federalTax.toFixed(2));
+    let totalTaxResult = federalTax + provincialTax;
+    console.log(totalTaxResult);
+    totalTaxResult = totalTaxResult.toFixed(2);
+    const calculatedSalary = (Number(income) - totalTaxResult).toFixed(2);
+
+    document.getElementById("federalTaxResult").innerHTML = "$" + federalTax;
+    document.getElementById("provincialTaxResult").innerHTML = "$" + provincialTax;
+    document.getElementById("incomeReminder").innerHTML = "$" + income;
+    document.getElementById("totalTaxResult").innerHTML = "$" + totalTaxResult;
+    document.getElementById("afterTaxSalary").innerHTML = "$" + calculatedSalary;
 
 }
